b5eed760f259c3d15e363c8a469c4f167e0bed3c
Somu3112 <88489136+Somu3112@users.noreply.github.com>
2021-08-13 10:16:46 +0530

    Package and access Spcifiers

---
+++ Somanshu Bendale/PackageDemo/OutDemoAccess.java
@@ -0,0 +1,9 @@
+import pckDemo.MummaAccess;
+
+public class OutDemoAccess extends MummaAccess
+{
+    public static void main(String[] args) {
+        MummaAccess m=new MummaAccess();
+        m.demo();
+    }
+}
